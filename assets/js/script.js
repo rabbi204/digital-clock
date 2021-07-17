0b93567fe7fc6abd8c847218ab19ac6e5b3a999a
rabbi204 <md.rabbi.web@gmail.com>
2021-07-17 10:47:19 +0600

    clock with progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let c = document.querySelector('#clock');
 let info = document.querySelector('#info');
 let value = document.querySelector('#value');
+let progress = document.querySelector('#progress-bar');
 // let y = document.querySelector('span #year');
 // let d = document.querySelector('span #date');
 // let m = document.querySelector('span #month');
@@ -10,18 +11,19 @@ let value = document.querySelector('#value');
 function clock(){
 
   let today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
+  let h = formatTime(today.getHours());
+  let m = formatTime(today.getMinutes());
+  let s = formatTime(today.getSeconds());
 
   let full_year = today.getFullYear();
-  let date = today.getDate();
-  let month = today.getMonth();
+  let date = formatTime(today.getDate());
+  let month = formatTime(today.getMonth());
   const x = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   let day = x[today.getDay()];
 
   let realHour;
   let am;
+
 
   if( h > 12 ){
     am = "PM";
@@ -30,31 +32,18 @@ function clock(){
     am = "AM";
     realHour = h;
   }
+  progress.style.width = (s/60) * 100 + "%";
 
   info.innerHTML = `${full_year} - ${month} - ${date} ${day}`;
   value.innerHTML = `${h} : ${m} : ${s} ${am}`;
 
-  // return `${realHour} : ${m} : ${s} ${am} ${month} - ${full_year} - ${day} `;
+}
 
-  // w.innerHTML = day;
-  // y.innerHTML = full_year;
-  // m.innerHTML = month;
-  // d.innerHTML = date;
-
-
+function formatTime(time) {
+ return time <10 ? `0${time}` : time; 
 }
 
 clock();
 setInterval( () => {
   clock.innerHTML = clock();
 }, 1000);
-
-
-// setTimeout function
-// setTimeout(function(){
-//   /////////statement
-// }, 1000);
-
-// btn_stop.onclick =() => {
-//   clearTimeout(countTimeout);
-// }
